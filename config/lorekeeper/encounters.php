348bd02b46d77ab50ca603dac10488f78cae4295
floatia <76548255+floatia@users.noreply.github.com>
2024-10-17 18:09:15 -0400

    no message

--- config/lorekeeper/encounters.php
+++ config/lorekeeper/encounters.php
@@ -15,7 +15,7 @@ return [
     //self explanatory
     //set to 1 to use characters instead of user stats
     //this will debit/grant or check things from the character instead
-    'use_characters' => 0, 
+    'use_characters' => 1, 
     //self explanatory
     //if turned off it will try to use currency instead
     'use_energy' => 1,
